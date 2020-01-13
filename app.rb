d11eeff06916839bda029a97fdc6fab5bbdeb660
Micah Levine <micah.levine@outlook.com>
2020-01-13 02:35:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
   end
 
   get "/square/:number" do
-    @number = params[:number] ** 2
-    "#{@number.to_s}"
+    @num = params[:number] ** 2
+    "#{@num.to_s}"
   end
 end
